4fd18c76d281fd081af5d771a2923a96d2c6c0f7
Rodrigo Hickenbick <rodrigo.hickenbick@gmail.com>
2023-01-11 10:43:15 +0000

    project planning completed

---
+++ README.md
@@ -0,0 +1,2 @@
+# rock-paper-scissores
+This is the popular rock, paper ans scissors made with HTML, CSS and JS proposed by Foundations course of The Odin Project.
---
+++ script.js
@@ -0,0 +1,22 @@
+// create computer choice over math random between 0 and 2
+// get computer choice create an string according to:
+// if computerChoice === 0 -> Store "Rock" on ComputerChoice
+// if computerChoice === 1 -> Store "Paper" on ComputerChoice
+// else -> "Paper" on ComputerChoice
+
+// get user choice over prompt
+// It must accept only "Rock", "Paper" or "Scissors" and it must receive capitals, uppercase and lowercase.
+// get computer choice create an string according to:
+// if computerChoice === 0 -> Store "Rock" on ComputerChoice
+// if computerChoice === 1 -> Store "Paper" on ComputerChoice
+// else -> "Paper" on ComputerChoice
+
+
+
+
+
+
+
+
+
+
