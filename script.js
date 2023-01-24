61c7e531317e2cd34773f1d6cd17119c434b5f5c
Rodrigo Hickenbick <rodrigo.hickenbick@gmail.com>
2023-01-24 16:14:46 +0000

    UI textContent added

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// text variables
+let introH2 = document.querySelector(".intro-h2");
+let introH3 = document.querySelector(".intro-h3");
+let userCounter = document.querySelector(".player-counter");
+let computerCounter = document.querySelector(".computer-counter");
+let userIcon = document.querySelector(".user-icon");
+let computerIcon = document.querySelector(".computer-icon");
+let btns = document.querySelector(".btn");
+
+// button variables
+const rockBtn = document.querySelector(".rock-btn") 
+const paperBtn = document.querySelector(".paper-btn") 
+const scissorBtn = document.querySelector(".scissor-btn") 
+
+// logic variables
 let computerScore = 0;
 let userScore = 0;
 let round = 1;
@@ -6,22 +21,41 @@ let round = 1;
 getComputerChoice = function() {
   randomNumber = Math.floor(Math.random() * 3)
   if (randomNumber === 0) {
-    return "rock"
+    computerIcon.textContent = "✊"
+    return "rock";
   } else if (randomNumber === 1) {
-    return "paper"
+    computerIcon.textContent = "✋"
+    return "paper";
   } else {
-    return "scissor"
+    computerIcon.textContent = "✌"
+    return "scissor";
   }
 }
 
 // Getting user choice through prompt, checking if fits "Rock", "Paper", or Scissor and returning it
 getUserChoice = function() {
   getWord = prompt("Please, write: Rock, Paper or Scissor", "")
-  userChoice = getWord.toLowerCase()
+  userChoice = getWord.toLowerCase();
+  printUserChoice(userChoice);
   if (userChoice === "rock" || userChoice === "paper" || userChoice === "scissor") {
-    return userChoice
+    return userChoice;
   } else {
     return alert("Incorrect input! Try again")
+  }
+}
+
+// Will print icon according to user choice
+function printUserChoice(userChoice) {
+  switch(userChoice) {
+    case "rock":
+      userIcon.textContent = "✊"
+    break
+    case "paper":
+      userIcon.textContent = "✋"
+    break
+    case "scissor":
+      userIcon.textContent = "✌"
+    break
   }
 }
 
@@ -37,18 +71,23 @@ function playRound(userChoice, computerChoice) {
   userChoice === "paper" && computerChoice === "rock" ||
   userChoice === "scissor" && computerChoice === "paper") {
   console.log("This round user wins!")
+  introH2.textContent = "This round user wins!"
   userScore++
+  userCounter.textContent = `${userScore}`;
   }
   if (
   userChoice === "rock" && computerChoice === "paper" || 
   userChoice === "paper" && computerChoice === "scissor" ||
   userChoice === "scissor" && computerChoice === "rock") {
   console.log("This round computer wins!")
+  introH2.textContent = "This round computer wins!"
   computerScore++
+  computerCounter.textContent = `${computerScore}`;
   }
   if (
     userChoice === computerChoice){
     console.log("This round is a tie!")
+    introH2.textContent = "This round is a tie!"
   }
   if (
     userChoice === undefined || userChoice === null) {
@@ -56,9 +95,10 @@ function playRound(userChoice, computerChoice) {
     round--
   }
   round++
+  introH3.textContent = `This is round ${round - 1} of 5`
   console.log("User score: " + userScore + " | " + "Computer score: " + computerScore)
   console.log("---- || -----")
-}
+} 
 
 // function with loop for 5 rounds announcing winner at the end and making variables 0 again
 function game() {
@@ -67,21 +107,36 @@ function game() {
   }
   if (userScore === computerScore) {
     console.log("The game finish is a TIE!")
+    introH2.textContent = "This game finish is a TIE!"
+    introH3.textContent = "You can't win against a simple code..."
   }
   if (userScore > computerScore) {
     console.log("USER wins the game!")
+    introH2.textContent = "USER wins the game!"
+    introH3.textContent = "Nothing really special..."
   } 
   if (userScore < computerScore) {
     console.log("COMPUTER wins the game!")
+    introH2.textContent = "COMPUTER wins the game!"
+    introH3.textContent = "Better try again loser..."
   } 
-  console.log("---- || -----")
+  console.log("----- || -----")
   round = 0
   userScore = 0
   computerScore = 0
-} 
+}
+
+btns.addEventListener("click", (btn) => {
+  player = btn.id
+  console.log(player);
+})
+
+
+
+
   
-// call for net game to start
-// game()
+
+
 
 
 
